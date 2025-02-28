2b002911b2767017f7170339a39c77d65414521c
KishanFowler <vogelaarkishan@gmail.com>
2025-02-28 09:13:10 +0100

    sprite

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,11 +9,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rigidBody;
     [SerializeField] private InputAction _playerInput;
     private Vector2 _movementDirection;
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] private List<Sprite> _sprites;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     void OnEnable()
@@ -29,6 +34,23 @@ public class PlayerMovement : MonoBehaviour
     {
         _movementDirection = Vector2.zero;
         _movementDirection += _playerInput.ReadValue<Vector2>();
+        if(_playerInput.ReadValue<Vector2>().normalized == new Vector2(0, 1))
+        {
+            _spriteRenderer.sprite = _sprites[2];
+        }
+        if (_playerInput.ReadValue<Vector2>().normalized == new Vector2(1, 0))
+        {
+            _spriteRenderer.sprite = _sprites[3];
+        }
+        if (_playerInput.ReadValue<Vector2>().normalized == new Vector2(0, -1))
+        {
+            _spriteRenderer.sprite = _sprites[0];
+        }
+        if (_playerInput.ReadValue<Vector2>().normalized == new Vector2(-1, 0))
+        {
+            _spriteRenderer.sprite = _sprites[1];
+        }
+
         _rigidBody.linearVelocity = _movementDirection.normalized * _movementSpeed;
     }
 }
