8bf0b8398bf414ad990d62fd9bbf0794f5b2eed8
KishanFowler <vogelaarkishan@gmail.com>
2025-03-03 15:31:10 +0100

    settings werken

--- Assets/Scripts/ScreenManager.cs
+++ Assets/Scripts/ScreenManager.cs
@@ -1,9 +1,12 @@
 using System;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ScreenManager : MonoBehaviour
 {
+
+    private Toggle _toggle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -11,6 +14,7 @@ public class ScreenManager : MonoBehaviour
         WaitForScreenChange(1920, 1080, true);
     }
 
+    // switches resolutions based on the selected options.
     public void ChangeScreenSize(TMPro.TMP_Dropdown dropdown)
     {
         switch(dropdown.value)
@@ -27,6 +31,7 @@ public class ScreenManager : MonoBehaviour
         }
     }
 
+    // this happens one frame after we toggle the resolution which gives unity enough time to apply it, doesnt work otherwise because of the time.
     public void WaitForScreenChange(int Width, int Height, bool Fullscreen)
     {
         Height = Screen.height;
@@ -35,12 +40,26 @@ public class ScreenManager : MonoBehaviour
         Screen.SetResolution(Width, Height, Fullscreen);
     }
 
+    //turns the opposite toggle off and toggles if were in fullscreen mode.
     public void ToggleFullScreen(bool Fullscreen) 
     {
         if (Fullscreen)
         {
-            //Screen.FullScreen = Fullscreen;
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+            _toggle.SetIsOnWithoutNotify(false);
         }
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+            _toggle.SetIsOnWithoutNotify(false);
+        }
+    }
+
+
+    //sets the toggles refrence.
+    public void SetToggleRefrence(Toggle toggle)
+    {
+        _toggle = toggle;
     }
 
 }
