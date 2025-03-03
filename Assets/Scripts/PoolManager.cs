77ead90140dd631b461ec559c50c9c4ec04d7f43
BrendanOssebaar <90763311+BrendanOssebaar@users.noreply.github.com>
2025-03-03 10:30:57 +0100

    Update PoolManager.cs

--- Assets/Scripts/PoolManager.cs
+++ Assets/Scripts/PoolManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class PoolManager : MonoBehaviour
 {
     public static PoolManager Instance;
-    private List<GameObject> roomPool = new List<GameObject>();
+    private readonly List<GameObject> _roomPool = new List<GameObject>();
 
     private void Awake()
     {
@@ -13,25 +13,25 @@ public class PoolManager : MonoBehaviour
 
     public void InitializePool(GameObject[] rooms)
     {
-        foreach (GameObject room in rooms)
+        foreach (var room in rooms)
         {
             room.SetActive(false);
-            roomPool.Add(room);
+            _roomPool.Add(room);
         }
     }
 
     public GameObject GetRandomRoom()
     {
-        if (roomPool.Count == 0) return null;
+        if (_roomPool.Count == 0) return null;
 
-        int randomIndex = Random.Range(0, roomPool.Count);
-        GameObject room = roomPool[randomIndex];
+        var randomIndex = Random.Range(0, _roomPool.Count);
+        var room = _roomPool[randomIndex];
 
         room.SetActive(true);
         return room;
     }
 
-    public void ReturnRoom(GameObject room)
+    public static void ReturnRoom(GameObject room)
     {
         room.SetActive(false);
     }
