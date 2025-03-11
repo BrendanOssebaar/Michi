acdbcff7a3c9057d5a70760a27afe8f9cc0126e6
KishanFowler <vogelaarkishan@gmail.com>
2025-03-11 14:57:51 +0100

    sanity bar geupdate

--- Assets/Scripts/SanitySystem.cs
+++ Assets/Scripts/SanitySystem.cs
@@ -23,21 +23,21 @@ public class SanitySystem : MonoBehaviour
         if (_player.GetComponent<Inventory>().hasPlush == true /*heeft plush*/ && _player.transform.GetChild(0).GetComponent<CandleBehaviour>().currentCandleState == 3 /*is uit*/)
         { 
             currentSanity = currentSanity - 0.33f;
-            sanityBar.rectTransform.sizeDelta = new Vector2(120, (currentSanity / 60));
+            sanityBar.rectTransform.sizeDelta = new Vector2(138, (currentSanity / 60));
             sanityText.text = (Mathf.Round(currentSanity / 60)).ToString();
             Debug.Log("wel plush, geen licht");
         }
         if (_player.GetComponent<Inventory>().hasPlush == false /*heeft plush niet */ && _player.transform.GetChild(0).GetComponent<CandleBehaviour>().currentCandleState < 3 /*staat aan*/)
         {
             currentSanity = currentSanity - 0.5f;
-            sanityBar.rectTransform.sizeDelta = new Vector2(120, (currentSanity / 60));
+            sanityBar.rectTransform.sizeDelta = new Vector2(138, (currentSanity / 60));
             sanityText.text = (Mathf.Round(currentSanity / 60)).ToString();
             Debug.Log("geen plush, wel licht");
         }
         if(_player.transform.GetChild(0).GetComponent<CandleBehaviour>().currentCandleState == 3 /*staat aan*/ && _player.GetComponent<Inventory>().hasPlush == false /*geen plush*/)
         {
             currentSanity = currentSanity - 2;
-            sanityBar.rectTransform.sizeDelta = new Vector2(120, (currentSanity / 60));
+            sanityBar.rectTransform.sizeDelta = new Vector2(138, (currentSanity / 60));
             sanityText.text = (Mathf.Round(currentSanity / 60)).ToString();
             Debug.Log("beide niet");
         }
